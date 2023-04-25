142819013f78a06d1f66f12a5c039d318451aba4
Carlos-Mantovani <sherifinho.augusto@gmail.com>
2023-04-24 22:04:44 -0300

    Turning all requests into just one route

--- main.py
+++ main.py
@@ -13,64 +13,94 @@ cursor = connection.cursor()
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route('/cars', methods=['GET'])
-def get_cars():
-	if 'id' in request.args():
-		id = request.args['id']
-		cursor.execute(f'SELECT * FROM cars WHERE id = {id}')
-		car = cursor.fetchall()
-		if cursor.rowcount != 0:
-			return make_response (
+@app.route('/cars', methods=['GET', 'POST', 'DELETE'])
+def cars():
+	if request.method == 'GET':
+		if 'id' in request.args:
+			id = request.args['id']
+			cursor.execute(f'SELECT * FROM cars WHERE id = {id}')
+			car = cursor.fetchall()
+			if cursor.rowcount != 0:
+				return make_response (
+					jsonify(
+						message='Car found',
+						data=car
+					)
+				)
+			else:
+				return make_response (
+					jsonify(
+						message='Car not found',
+					)
+				), 404
+		else:
+			cursor.execute(f'SELECT * FROM cars')
+			cars = cursor.fetchall()
+			return make_response(
 				jsonify(
-					message='Car found',
-					data=car
+					message='Car list',
+					data=cars
 				)
 			)
-		else:
-			return make_response (
-				jsonify(
-					message='Car not found',
-				)
-			), 404
-	else:
-		cursor.execute(f'SELECT * FROM cars')
-		cars = cursor.fetchall()
+	elif request.method == 'POST':
+		car = request.json
+		print(car)
+		cursor.execute(f'INSERT INTO cars (brand, model, year) VALUES (%s, %s, %s)', (car['brand'], car['model'], car['year']))
+		connection.commit()
 		return make_response(
 			jsonify(
-				message='Car list',
-				data=cars
+				message='Car registered with sucess',
+				data=car
 			)
 		)
-
-@app.route('/cars', methods=['POST'])
-def create_car():
-	car = request.json
-	print(car)
-	cursor.execute(f'INSERT INTO cars (brand, model, year) VALUES (%s, %s, %s)', (car['brand'], car['model'], car['year']))
-	connection.commit()
-	return make_response(
-		jsonify(
-			message='Car registered with sucess',
-			data=car
-		)
-	)
-
-@app.route('/cars', methods=['DELETE'])
-def delete_car():
-	if 'id' in request.args:
-		id = request.args['id']
-		cursor.execute(f'DELETE FROM cars WHERE id = {id}')
-		car = cursor.fetchall()
-		if cursor.rowcount != 0:
-			return make_response (
-				jsonify(
-					message='Car deleted'
+	elif request.method == 'DELETE':
+		if 'id' in request.args:
+			id = request.args['id']
+			cursor.execute(f'DELETE FROM cars WHERE id = {id}')
+			car = cursor.fetchall()
+			if cursor.rowcount != 0:
+				return make_response (
+					jsonify(
+						message='Car deleted'
+					)
 				)
-			)
-		else:
-			return make_response (
-				jsonify(
-					message='Car not found'
-				)
-			), 404
+			else:
+				return make_response (
+					jsonify(
+						message='Car not found'
+					)
+				), 404
+
+# @app.route('/cars', methods=['POST'])
+# def create_car():
+# 	car = request.json
+# 	print(car)
+# 	cursor.execute(f'INSERT INTO cars (brand, model, year) VALUES (%s, %s, %s)', (car['brand'], car['model'], car['year']))
+# 	connection.commit()
+# 	return make_response(
+# 		jsonify(
+# 			message='Car registered with sucess',
+# 			data=car
+# 		)
+# 	)
+
+# @app.route('/cars', methods=['DELETE'])
+# def delete_car():
+# 	if 'id' in request.args:
+# 		id = request.args['id']
+# 		cursor.execute(f'DELETE FROM cars WHERE id = {id}')
+# 		car = cursor.fetchall()
+# 		if cursor.rowcount != 0:
+# 			return make_response (
+# 				jsonify(
+# 					message='Car deleted'
+# 				)
+# 			)
+# 		else:
+# 			return make_response (
+# 				jsonify(
+# 					message='Car not found'
+# 				)
+# 			), 404
+
 app.run()
